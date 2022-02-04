62966a31aa9de48b02599d3e064befb53191e15b
Vadim Egorov <egorovvadim2001@gmail.com>
2022-02-04 17:25:35 +0400

    Init

---
+++ make.bat
@@ -0,0 +1,4 @@
+tasm.exe mopl1.asm
+tasm.exe mopl1l.asm
+tlink.exe mopl1.obj + mopl1l.obj
+mopl1.exe
---
+++ mopl1.asm
@@ -0,0 +1,140 @@
+;***************************************************************************************************
+; MOPL1.ASM - учебный пример для выполнения 
+; лабораторной работы N1 по машинно-ориентированному программированию
+;***************************************************************************************************
+        .MODEL SMALL
+        .STACK 200h
+	.386
+;       Используются декларации констант и макросов
+        INCLUDE MOPL1.INC	
+        INCLUDE MOPL1.MAC
+
+; Декларации данных
+        .DATA    
+SLINE	DB	78 DUP (CHSEP), 0
+REQ	DB	"Fimiliya I.O.: ",0FFh
+MINIS	DB	"MINISTERSTVO OBRAZOVANIYA ROSSIJSKOJ FEDERACII",0
+ULSTU	DB	"UL'YANOVSKIJ GOSUDARSTVENNYJ TEKHNICHESKIJ UNIVERSITET",0
+DEPT	DB	"Kafedra vychislitel'noj tekhniki",0
+MOP	DB	"Mashinno-orientirovannoe programmirovanie",0
+LABR	DB	"Laboratornaya rabota N 1",0
+Choc	DB	"Calculate?", 0
+REQ1    DB      "Zamedlit' vremya raboty v taktah(-), uskorit' vremya raboty v taktah (+),", 0
+;------------- Новые переменные ------------------------------------------------------------------
+REQ2	DB	"vychislit' funkciyu (f), vyjti(ESC)?", 0
+;-------------------------------------------------------------------------------------------------
+TACTS   DB	"Время работы в тактах: ",0
+X DB 0,"X = "
+XNUM  DB 0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0
+Y DB "Y = "
+YNUM  DB 0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0
+EMPTYS	DB	0
+BUFLEN = 70
+BUF	DB	BUFLEN
+LENS	DB	?
+SNAME	DB	BUFLEN DUP (0)
+PAUSE	DW	0, 0 ; младшее и старшее слова задержки при выводе строки
+TI	DB	LENNUM+LENNUM/2 DUP(?), 0 ; строка вывода числа тактов
+                                          ; запас для разделительных "`"
+
+;========================= Программа =========================
+        .CODE
+; Макрос заполнения строки LINE от позиции POS содержимым CNT объектов,
+; адресуемых адресом ADR при ширине поля вывода WFLD
+BEGIN	LABEL	NEAR
+	; инициализация сегментного регистра
+	MOV	AX,	@DATA
+	MOV	DS,	AX
+	; инициализация задержки
+	MOV	PAUSE,	PAUSE_L
+	MOV	PAUSE+2,PAUSE_H
+	PUTLS	REQ	; запрос имени
+	; ввод имени
+	LEA	DX,	BUF
+	CALL	GETS	
+@@L:	; циклический процесс повторения вывода заставки
+	; вывод заставки
+	; ИЗМЕРЕНИЕ ВРЕМЕНИ НАЧАТЬ ЗДЕСЬ
+	FIXTIME
+	PUTL	EMPTYS
+	PUTL	SLINE	; разделительная черта
+	PUTL	EMPTYS
+	PUTLSC	MINIS	; первая 
+	PUTL	EMPTYS
+	PUTLSC	ULSTU	;  и  
+	PUTL	EMPTYS
+	PUTLSC	DEPT	;   последующие 
+	PUTL	EMPTYS
+	PUTLSC	MOP	;    строки  
+	PUTL	EMPTYS
+	PUTLSC	LABR	;     заставки
+	PUTL	EMPTYS
+	; приветствие
+	PUTLSC	SNAME   ; ФИО студента
+	PUTL	EMPTYS
+	; разделительная черта
+	PUTL	SLINE
+	; ИЗМЕРЕНИЕ ВРЕМЕНИ ЗАКОНЧИТЬ ЗДЕСЬ 
+	DURAT    	; подсчет затраченного времени
+	; Преобразование числа тиков в строку и вывод
+	LEA	DI,	TI
+	CALL	UTOA10	
+	PUTL	TACTS
+	PUTL	TI      ; вывод числа тактов
+	; обработка команды
+	PUTL	REQ1
+;------Вывод своих строк с действиями -------------------
+	PUTL	REQ2
+;--------------------------------------------------------
+	CALL	GETCH
+	CMP	AL,	'-'    ; удлиннять задержку?
+	JNE	CMINUS
+	INC	PAUSE+2        ; добавить 65536 мкс
+	JMP	@@L
+CMINUS:	CMP	AL,	'+'    ; укорачивать задержку?
+	JNE	CEXIT
+	CMP	WORD PTR PAUSE+2, 0		
+	JE	BACK
+	DEC	PAUSE+2        ; убавить 65536 мкс
+BACK:	JMP	@@L
+CEXIT:	CMP	AL,	'f'
+	JNE	CFUNC
+	JMP func
+CFUNC: CMP	AL,	CHESC
+	JE	@@E
+	TEST	AL,	AL
+	JNE	BACK
+	CALL	GETCH
+	JMP	@@L
+	; Выход из программы
+func:
+	PUTLS X
+	CALL	GETCH
+	SUB AL, 30h ;введение позиции бита
+	mov cl, AL  ;кол-во сдвигов
+	ror ebx, cl ;сдвиг под  нужный бит
+	CALL GETCH
+	SUB AL, 30h ;запись значения
+	add bl, AL	;прибавление значения
+	mov al, 32h ;длина ebx
+	sub al, cl  ;получем значение для возвращения числа на исходную позицию
+	mov cl, al	;перемещаем значение в cl
+	ror ebx, cl ;сдвигаем в начальное состояние
+	JMP CHOICE
+CHOICE:
+	mov dl,al
+	mov ah,02h
+	int 21h
+	PUTLS Choc
+	CALL GETCH
+	CMP	al, 'y'
+	JNE func
+
+@@E:	EXIT	
+        EXTRN	PUTSS:  NEAR
+        EXTRN	PUTC:   NEAR
+	EXTRN   GETCH:  NEAR
+	EXTRN   GETS:   NEAR
+	EXTRN   SLEN:   NEAR
+	EXTRN   UTOA10: NEAR
+	END	BEGIN
